87784cf9544a5e1ede930a22119ff0fcc4c979a7
BrokiDev <bryantramirez855@gmail.com>
2024-02-19 11:29:16 -0400

    [feature]- create the update endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,14 @@
 import express from "express";
 import fs from 'fs'
 
-// const express = require("express");
 
 const app = express();
 const port = 1234;
 
 
-
-
 const tours =JSON.parse(fs.readFileSync(`dev-data/data/tours-simple.json`,'utf-8'))
 
-
-
-
-
-
-// app.get('/', (req, res)=> {
-//     res.status(200).json({
-//         message: 'Hello from the server side!',
-//         app: 'Natours'
-//     })
-// })
-
-// app.post('/',({req,res}:any)=> {
-//     res.status(200).send('<h1>you cant post here</h1>')
-// })
+app.use(express.json())
 
 
 app.get('/api/v1/tours',(req,res)=> {
@@ -35,6 +18,23 @@ app.get('/api/v1/tours',(req,res)=> {
         data: {
             tours
         }
+    })
+})
+
+app.post('/api/v1/tours',(req,res)=> {
+    // console.log(req.body)
+    const newId = tours[tours.length -1].id ++;
+    const newTour = Object.assign({id:newId},req.body)
+
+    tours.push(newTour)
+
+    fs.writeFile(`dev-data/data/tours-simple.json`,JSON.stringify(tours),(err)=> {
+        res.status(201).json({
+            status:'success',
+            data: {
+                tours:newTour
+            }
+        })
     })
 })
 
